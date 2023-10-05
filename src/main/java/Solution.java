d2f182767b5e1e679130ae41dfffacbcaff650dd
alek <wait82985@gmail.com>
2023-10-05 12:30:02 +0600

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,7 +1,24 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        return null;
+        int target = nums.length / 3;
+        List<Integer> result = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int x: nums) {
+            int count = map.getOrDefault(x, 0);
+            count++;
+            if(count - 1 == target){
+                result.add(x);
+                if(result.size() == 2){
+                    break;
+                }
+            }
+            map.put(x, count);
+        }
+        return result;
     }
 }
